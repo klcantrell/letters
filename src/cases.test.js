9d9058aad36f34782c06827a81956e755767ef9a
Kalalau <cantrellkalalau@gmail.com>
2018-10-29 22:58:23 -0400

    add test cases

--- src/cases.test.js
+++ src/cases.test.js
@@ -1,5 +1,18 @@
-describe('checking this test', () => {
-  it('work', () => {
-    expect('A').toBe('A');
+const buildDiamond = require('./app');
+
+describe('test cases for the buildDiamond function', () => {
+  test('A', () => {
+    console.log(buildDiamond('A'));
+    expect(buildDiamond('A').trim()).toBe('A'.trim());
+  });
+  test('B', () => {
+    console.log(buildDiamond('B'));
+    expect(buildDiamond('B').trim()).toBe(' A \nB B\n A '.trim());
+  });
+  test('C', () => {
+    console.log(buildDiamond('C'));
+    expect(buildDiamond('C').trim()).toBe(
+      '  A  \n B B \nC   C\n B B \n  A  '.trim()
+    );
   });
 });
